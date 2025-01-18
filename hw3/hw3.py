3821563ccc03323ed35d551c27021c07240ce08e
Suhail Rehman <suhailrehman@gmail.com>
2025-01-18 08:51:34 -0600

    HW3 release

---
+++ hw3/hw3.py
@@ -0,0 +1,131 @@
+"""
+CMSC 14100
+Winter 2025
+Homework #3
+
+We will be using anonymous grading, so please do NOT include your name
+in this file
+
+People Consulted:
+   List anyone (other than the course staff) that you consulted about
+   this assignment.
+
+Online resources consulted:
+   List the URL of any online resources other than the course text and
+   the official Python language documentation that you used to complete
+   this assignment.
+"""
+
+##################################################
+#                                                #
+# Important note: some of the tasks in this      #
+#  assignment have task-specific                 #
+#  reqirements/restrictions concerning           #
+#  the language constructs that you are          #
+#  allowed to use in your solution.  See the     #
+#  assignment writeup for details.               #
+#                                                #
+##################################################
+
+# Some useful constants
+NORMAL = 0
+PREDIABETES = 1
+DIABETES = 2
+NEITHER = -1
+
+
+# Exercise 1
+def categorize_a1c(a1c):
+    """
+    Given the A1C concentration, categorize the A1C concentration
+    according to the following table:
+
+    A1C Category          A1C Range
+    NORMAL                Below 5.7%
+    PREDIABETES           5.7% to below 6.5%
+    DIABETES              6.5% and above
+
+    Args:
+        a1c (float): the A1C concentration
+
+    Returns (int): the category of the A1C concentration
+    """
+    ### EXERCISE 1 -- YOUR CODE GOES HERE
+    ### Replace the following line with your code
+    return None
+
+def which_comes_first(scores):
+    """
+    Your docstring goes here
+    """
+    ### EXERCISE 2 -- YOUR CODE GOES HERE
+    ### Replace the following line with your code
+    return None
+
+
+# Exercise 3
+def which_comes_last(scores):
+    """
+    Your docstring goes here
+    """
+    ### EXERCISE 3 -- YOUR CODE GOES HERE
+    ### Replace the following line with your code
+    return None
+
+
+# Exercise 4
+def is_multiples_of_first(scores):
+    """
+    Your docstring goes here
+    """
+    ### EXERCISE 4 -- YOUR CODE GOES HERE
+    ### Replace the following line with your code
+    return None
+
+# Exercise 5
+def violation_points(violation_type, location):
+    """
+    Your docstring goes here
+    """
+    ### EXERCISE 5 -- YOUR CODE GOES HERE
+    ### Replace the following line with your code
+
+#Exercise 6
+def is_suspended(violations, driver_age):
+    """
+    Your docstring goes here
+    """
+
+    ### EXERCISE 6 -- YOUR CODE GOES HERE
+    ### Replace the following line with your code
+    return None
+
+
+# Exercise 7
+def valid_grades(grades):
+    """
+    Your docstring goes here
+    """
+    ### EXERCISE 7 -- YOUR CODE GOES HERE
+    ### Replace the following line with your code
+    return None
+
+# Exercise 8
+def technical_element_score(base_value, grades):
+    """
+    Your docstring goes here
+    """
+    ### EXERCISE 8 -- YOUR CODE GOES HERE
+    ### Replace the following line with your code
+    return None
+
+
+# Exercise 9
+# Restriction: you may not use the index method in your solution for this task.
+def get_first_non_normal(scores):
+    """
+    Your docstring goes here
+    """
+    ### EXERCISE 9 -- YOUR CODE GOES HERE
+    ### Replace the following line with your code
+    return None
